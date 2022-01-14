dea393e1f058e7c5ebf90eaa6a9560085bc6bfe6
DtHnAme <1904685725@qq.com>
2022-01-14 15:10:27 +0800

    initial edgeration build

--- core/config.mk
+++ core/config.mk
@@ -317,8 +317,8 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+ifneq ($(EDGERATION_BUILD),)
+include vendor/edger/config/BoardConfigEdgeration.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1163,7 +1163,7 @@ dont_bother_goals := out \
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(EDGERATION_BUILD),)
 ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
